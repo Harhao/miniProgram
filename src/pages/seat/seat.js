7d8970801bb9261aaf0b93e1027844fc4e1bc177
huahaoworkspace@163.com <huahaoworkspace@163.com>
2018-08-08 17:56:20 +0800

    huahao

--- src/pages/seat/seat.js
+++ src/pages/seat/seat.js
@@ -10,8 +10,8 @@ export default class Seat extends Component {
     super(props);
     this.state = {
       seatData:[],
-      statusMap:["http://p1.meituan.net/movie/9dfff6fd525a7119d44e5734ab0e9fb41244.png","http://p1.meituan.net/movie/bdb0531259ae1188b9398520f9692cbd1249.png","http://p0.meituan.net/movie/585588bd86828ed54eed828dcb89bfdd1401.png"],
-      icon:'http://p1.meituan.net/movie/9dfff6fd525a7119d44e5734ab0e9fb41244.png',
+      statusMap:["https://p1.meituan.net/movie/9dfff6fd525a7119d44e5734ab0e9fb41244.png","https://p1.meituan.net/movie/bdb0531259ae1188b9398520f9692cbd1249.png","https://p0.meituan.net/movie/585588bd86828ed54eed828dcb89bfdd1401.png"],
+      icon:'https://p1.meituan.net/movie/9dfff6fd525a7119d44e5734ab0e9fb41244.png',
       active:'0'
     }
 
@@ -20,7 +20,7 @@ export default class Seat extends Component {
     const params = this.$router.params;
     const self = this;
     Taro.request({
-      url:`http://m.maoyan.com/ajax/seatingPlan?cityId=${params.cityId}&ci=${params.ci}&seqNo=${params.seqNo}`,
+      url:`https://m.maoyan.com/ajax/seatingPlan?cityId=${params.cityId}&ci=${params.ci}&seqNo=${params.seqNo}`,
       method:'GET',
       header:{
         'Cookie': '_lxsdk_cuid=164b6cae2cac8-02b7b032f571b5-39614706-1fa400-164b6cae2cbc8; v=3; iuuid=1A6E888B4A4B29B16FBA1299108DBE9CA19FF6972813B39CA13A8D9705187374; ci=20%2C%E5%B9%BF%E5%B7%9E; selectci=; __mta=3463951.1532075108184.1533368636504.1533516565814.26; _lxsdk=1A6E888B4A4B29B16FBA1299108DBE9CA19FF6972813B39CA13A8D9705187374; __mta=3463951.1532075108184.1533516565814.1533629089523.27"; webp=true; uid=124265875; token=gcPnmyWgpw-Lw42L2Kyh8heCJRUAAAAAPAYAACytLQksSUCHw2tGR-dVok-gMM_PAJAddxxzMcwBMvafGuiBJ3PybndW14H1q_ueQw; from=canary; __mta=3463951.1532075108184.1533629096583.1533691360790.28; _lxsdk_s=1651720dbe9-cdd-1c3-614%7C%7C10; user=124265875%2CgcPnmyWgpw-Lw42L2Kyh8heCJRUAAAAAPAYAACytLQksSUCHw2tGR-dVok-gMM_PAJAddxxzMcwBMvafGuiBJ3PybndW14H1q_ueQw'
