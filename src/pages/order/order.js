120f3e4b53ff0c12533c2bd8aaf3bb74ad613f8a
kiwis <huahaoworkspace@163.com>
2018-08-10 00:17:44 +0800

    feat:add order

---
+++ src/pages/order/order.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Administrator on 2018/8/10 0010.
+ */
+import Taro, { Component } from '@tarojs/taro'
+import { View, Text } from '@tarojs/components'
+import './order.scss'
+export default class Map extends Component {
+  constructor(props){
+    super(props);
+    this.state = {}
+
+  }
+
+  componentDidMount () {
+  }
+  render () {
+    return (
+      <View className="order">
+
+      </View>
+    )
+  }
+}
