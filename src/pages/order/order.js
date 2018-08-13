a7939f331842d5999cf8a1d70d45c28f316feae9
huahaoworkspace@163.com <huahaoworkspace@163.com>
2018-08-13 10:58:55 +0800

    feature:order:添加订单啊模块

--- src/pages/order/order.js
+++ src/pages/order/order.js
@@ -7,12 +7,12 @@ import './order.scss'
 export default class Map extends Component {
   constructor(props){
     super(props);
-    this.state = {}
+    this.state = {
+    }
   }
   config = {
     navigationBarTitleText: '确认订单'
   }
-
   componentDidMount () {
   }
   render () {
@@ -22,6 +22,34 @@ export default class Map extends Component {
           支付剩余时间:
           <View className="restTime">14:00</View>
         </View>
+        <View className="movieInfo">
+          <View className="movieName">西红柿首富</View>
+          <View className="movieTime">今天8月13日 11:10 (英语3D)</View>
+          <View className="cinemas">中影凤凰影城(同和店) </View>
+          <View className="station">Dmax 全景声2厅</View>
+        </View>
+        <View className="discountInfo">
+          <View className="card">
+            <View className="name">活动与抵用券</View>
+            <View className="orNot">
+              <Text className="useful">无可用</Text>
+              <Text className="arrow"></Text>
+            </View>
+          </View>
+          <View className="phone">
+            <View className="name">手机号码</View>
+            <View className="number">
+              13415334317
+            </View>
+          </View>
+          <View className="totalMoney">
+            <View className="name">小计</View>
+            <View className="total">
+              ￥41.9
+            </View>
+          </View>
+        </View>
+        <View className="line"></View>
       </View>
     )
   }
