928fecd88463540675c937c4b5b85ff6dfab1dd2
huahaoworkspace@163.com <huahaoworkspace@163.com>
2019-04-02 20:25:56 +0800

    delete the comment

--- src/app.js
+++ src/app.js
@@ -1,67 +1,68 @@
-import Taro, { Component } from '@tarojs/taro'
-import Movies from './pages/movies/movies'
-import './app.scss'
+import Taro, { Component } from "@tarojs/taro";
+import Movies from "./pages/movies/movies";
+import "./app.scss";
 class App extends Component {
   config = {
     pages: [
-      'pages/movies/movies',
-      'pages/person/person',
-      'pages/cinema/cinema',
-      'pages/position/position',
-      'pages/search/search',
-      'pages/detail/detail',
-      'pages/content/content',
-      'pages/cinemaDetail/cinemaDetail',
-      'pages/map/map',
-      'pages/seat/seat',
-      'pages/user/user',
-      'pages/order/order'
+      "pages/movies/movies",
+      "pages/person/person",
+      "pages/cinema/cinema",
+      "pages/position/position",
+      "pages/search/search",
+      "pages/detail/detail",
+      "pages/content/content",
+      "pages/cinemaDetail/cinemaDetail",
+      "pages/map/map",
+      "pages/seat/seat",
+      "pages/user/user",
+      "pages/order/order"
     ],
     window: {
-      backgroundTextStyle: 'light',
-      navigationBarBackgroundColor: '#e54847',
-      navigationBarTitleText: '猫眼电影',
-      navigationBarTextStyle: 'white',
-      enablePullDownRefresh:true,
+      backgroundTextStyle: "light",
+      navigationBarBackgroundColor: "#e54847",
+      navigationBarTitleText: "猫眼电影",
+      navigationBarTextStyle: "white",
+      enablePullDownRefresh: true
     },
-    tabBar:{
-      color:"#333",
-      selectedColor:"#f03d37",
-      backgroundColor: '#fff',
-      borderStyle: 'black',
-      list:[{
-        pagePath:"pages/movies/movies",
-        text:"电影",
-        iconPath: './assets/images/index.png',
-        selectedIconPath: './assets/images/index_focus.png'
-      },{
-        pagePath:"pages/cinema/cinema",
-        text:"影院",
-        iconPath: './assets/images/themeOld.png',
-        selectedIconPath: './assets/images/theme.png'
-      },{
-        pagePath:"pages/person/person",
-        text:"我的",
-        iconPath: './assets/images/person.png',
-        selectedIconPath: './assets/images/personSelect.png'
-      }]
+    tabBar: {
+      color: "#333",
+      selectedColor: "#f03d37",
+      backgroundColor: "#fff",
+      borderStyle: "black",
+      list: [
+        {
+          pagePath: "pages/movies/movies",
+          text: "电影",
+          iconPath: "./assets/images/index.png",
+          selectedIconPath: "./assets/images/index_focus.png"
+        },
+        {
+          pagePath: "pages/cinema/cinema",
+          text: "影院",
+          iconPath: "./assets/images/themeOld.png",
+          selectedIconPath: "./assets/images/theme.png"
+        },
+        {
+          pagePath: "pages/person/person",
+          text: "我的",
+          iconPath: "./assets/images/person.png",
+          selectedIconPath: "./assets/images/personSelect.png"
+        }
+      ]
     }
-  }
+  };
 
-  componentDidMount () {
-  }
+  componentDidMount() {}
 
-  componentDidShow () {}
+  componentDidShow() {}
 
-  componentDidHide () {}
+  componentDidHide() {}
 
-  componentCatchError () {}
+  componentCatchError() {}
 
-  render () {
-    return (
-      <Movies />
-    )
+  render() {
+    return <Movies />;
   }
 }
 
-Taro.render(<App />, document.getElementById('app'))
+Taro.render(<App />, document.getElementById("app"));
